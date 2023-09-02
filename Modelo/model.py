ff6f210bf295c5881a19396953fe73f2eb12e866
juansala305 <je.salamanca@uniandes.edu.co>
2023-09-02 15:17:16 -0500

    Primer Commit :D

--- Modelo/model.py
+++ Modelo/model.py
@@ -1,1 +1,13 @@
+import regex as re 
 
+filename = "Data/prueba.txt"
+file = open(filename,"r",encoding="utf-8")
+text = file.read()
+raw_tokenizer = re.split("\s",text)
+
+tokenizer = []
+separadores = ["(",")","{","}"]
+
+    
+        
+        
