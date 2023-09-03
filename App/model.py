db14c119a33c7396dc009bd099c4b01831e57140
juansala305 <je.salamanca@uniandes.edu.co>
2023-09-03 10:52:49 -0500

    changes tokenizer

--- App/model.py
+++ App/model.py
@@ -16,12 +16,34 @@ def verify_code(text:str)->bool:
 
 def tokenize(text:str)->list:
     
-    
     raw_list = re.split("\s",text)
     tokenized_list = []
     
     for token in raw_list:
+        
+        if token!= "":
+            if has_separator(token):
+                pass
+                
+            else:
+                tokenized_list.append(token)
+        
+        
         pass
 
     return True
-        
+
+def has_separator(token:str)->bool:
+    """_summary_
+
+    Args:
+        token (str): Any token in form of str
+
+    Returns:
+        bool: Returns if a separator is present in the token
+    """
+    
+    if ("(") in token or (")") in token or ("{") in token or ("}") in token:
+        return True
+    else:
+        return False
