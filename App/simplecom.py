abb938cb16234f16e299d8e99d3b5a84e171c718
LuigiEnLaCasa <l.pinillag@uniandes.edu.co>
2023-09-03 15:54:38 -0500

    fasga

--- App/simplecom.py
+++ App/simplecom.py
@@ -24,7 +24,26 @@ can
 
 
 """
+regular_command_data = {
+    "jump":{"Quantity":2 ,"Types": [int,int]},
+    "turn":{"Quantity":1 , "Types": [str]},
+    "turnto":{"Quantity":1 , "Types": [str]},
+    "drop":{"Quantity":1 , "Types": [int]},
+    "get":{"Quantity":1 , "Types": [int]},
+    "grab":{"Quantity":1 , "Types": [int]},
+    "letgo": {"Quantity":1 , "Types": [int]},
+    #"nop":{"Quantity":0 , "Types": [None]},
+    "facing": {"Quantity":1 , "Types": [str]},
+    "can":{"Quantity":1 , "Types": [str]}
+    #"not": {"Quantity":1 , "Types": [bool]}
+    }
 
+special_command_data = {
+    "walk":{"Quantity"},
+    "leap": {}
+
+
+}
 
 allowed_D = ["front", "right", "left","back"]
 allowed_O= ["north", "south", "west", "east"]
@@ -35,9 +54,10 @@ def commandSyntaxChecker(sublist:list)->bool:
 
 
     if sublist[0] == "walk" or sublist[0]== "leap":
-        checkSpecialCommand(sublist)
+        return checkSpecialCommand(sublist)
     else:
-        checkRegularCommand(sublist)
+        
+        return checkRegularCommand(sublist)
 
 
 
@@ -51,24 +71,25 @@ def checkRegularCommand(sublist:list)->bool:
     Return: True/False if the syntax is correct. 
     """
     
-    criteriaParameter = {"Quantity":2, "Types": (int,int)}
+    #Checks if the structure of the task starts with kw followed by ( and ends in )
+    if sublist[0] not in regular_command_data.keys() or sublist[1]!= "(" or sublist[-1]!= ")":
+            return False
+
+
+    criteriaParameter = regular_command_data[sublist[0]]
     parameters = []
     structure = True
     while structure:
 
-        #Checks if the structure of the task starts with kw followed by ( and ends in )
-        if sublist[0] != "jump" or sublist[1]!= "(" or sublist[-1]!= ")":
-            structure = False
         #Makes a sub-sub list with the parameters so I can check them
-        elif sublist[1] == "(":
             parameters = sublist[2:-1]
             #This function checks that the parameters are correctly indexed.
             if parameterVerifier(parameters, criteriaParameter) !=True:
                 structure = False
             else: 
-                print("hasta aquihemos revisado que abra y cierrre con parentessis y que los parametros estén separados por coma")
-
-            
+                print("Hola")
+                return True
+                
 
     return structure
 
@@ -82,21 +103,28 @@ def checkSpecialCommand(sublist:list)->bool:
 def parameterVerifier(paramtoVerify:list, criteria:dict)->bool:
     """
     This function sets a set of rules for all the parameters for the simple commands and checks if they check out
+    Args: Parameter to verifu(list) it is a token list with the components inside a parameter instantation
+    return : True or false if the parameter lists are well defined. 
     """
 
     estructParam = True
     while estructParam:
         #This function checks if the parameters are correctly separated by commas
-        if isSeparatedbyComma(paramtoVerify) != True:
-            estructParam = False
+        commas=isSeparatedbyComma(paramtoVerify)
+        if commas != True:
+            
+            return False
         
-        if correctNumberParameters(paramtoVerify, criteria) !=True:
-            estructParam = False
+        num_parameters = correctNumberParameters(paramtoVerify, criteria)
+        if  num_parameters!=True:
+            
+            return False
         
-        if correctTypeParams(paramtoVerify, criteria) != True:
+        if correctTypeParamsRegular(paramtoVerify, criteria) != True:
             estructParam = False
 
         return True
+    
     return estructParam
 
 
@@ -114,14 +142,12 @@ def isSeparatedbyComma(paramtoVerify:list)->bool:
         if num_Notcommas- num_commas ==1:
             return True
         
-
         return True
 
 def correctNumberParameters(paramtoVerify:list, criteria:dict)->bool:
     
 
     only_param = [parameter for parameter in paramtoVerify if parameter != ","]
-
     if len(only_param) == criteria["Quantity"]:
         return True
     
@@ -130,18 +156,40 @@ def correctNumberParameters(paramtoVerify:list, criteria:dict)->bool:
     
 
 
-def correctTypeParams(paramtoVerify: list, criteria: dict)->bool:
+def correctTypeParamsRegular(paramtoVerify: list, criteria: dict)->bool:
+    """This will check if the parameters entered into the  function are coherent to the ones 
+    specified on the language for functoins other than walk or leap"""
     
     only_parameters = [parameter for parameter in paramtoVerify if parameter != ","]
-    print(only_parameters)
-    
-    return 0
+    criteria_types = criteria["Types"]
+
+    fixed_parameters = []
+
+    for p in only_parameters:
+        try:
+            p_int = int(p)
+            fixed_parameters.append(p_int)
+        
+        except ValueError:
+            fixed_parameters.append(p)
+
+
+
+    for c in range(0,len(fixed_parameters)):
+        if type(fixed_parameters[c])!= criteria_types[c]:
+            return False
+        else:
+            if type(fixed_parameters[c]) == str: 
+                if fixed_parameters not in allowed_D or fixed_parameters not in allowed_O: 
+                    return False
+
+    return True
 
 ##############################
 ##############################
 
-prueba = ["jump","(","x",",","y",")"]
-print(checkRegularCommand(prueba))
+prueba = ["drop","(","x",")"]
+print(commandSyntaxChecker(prueba))
 
 
 
